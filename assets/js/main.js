f104c4d0e49d51623eb69c253caed9079439fabd
Danielateresa <dany.t_87@hotmail.it>
2022-10-07 16:07:49 +0200

    working on click function

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,31 +13,34 @@ Il riepilogo dei dati e l'output del prezzo finale, andranno quindi stampati in 
 Questo richiederà un minimo di ricerca.
 */
 
-
-//chiedere all'utente i km da percorrere
-const km = document.querySelector('distance');
-console.log(km);
-
-//chiedere all'utente l'età del passeggero
-const passengerAge = document.querySelector('age');
-console.log(passengerAge);
-
-//calcolare il costo del biglietto in base ai km (0.21 € al km)
-const price = km * 0.21;
-//va applicato uno sconto 10del 20% per i minorenni
-discountMinors = price * 0.20;
-priceMinors = price - discountMinors;
-//va applicato uno sconto del 40% per gli over 65.
-discountOver65 = price * 0.40;
-priceOver65 = price - discountOver65;
+const resultButton = document.getElementById('button');
+resultButton.addEventListener('click', function () {
+    //console.log('hai cliccato');
+    const km = document.getElementById('distance').value;
+    console.log(km);
+    const price = km * 0.21;
+})
 
 
-if (passengerAge < 18) {
-    console.log("Dato che sei minorenne, il tuo biglietto scontato costerà €" + priceMinors);
-} else if (passengerAge > 65) {
-    console.log("Dato che hai più di 65 anni, il tuo biglietto scontato costerà €" + priceOver65);
-} else {
-    console.log("Il tuo biglietto costerà €" + price);
-}
+
+// console.log(passengerAge);
+
+// //calcolare il costo del biglietto in base ai km (0.21 € al km)
+//
+// //va applicato uno sconto 10del 20% per i minorenni
+// discountMinors = price * 0.20;
+// priceMinors = price - discountMinors;
+// //va applicato uno sconto del 40% per gli over 65.
+// discountOver65 = price * 0.40;
+// priceOver65 = price - discountOver65;
+
+
+// if (passengerAge < 18) {
+//     console.log("Dato che sei minorenne, il tuo biglietto scontato costerà €" + priceMinors);
+// } else if (passengerAge > 65) {
+//     console.log("Dato che hai più di 65 anni, il tuo biglietto scontato costerà €" + priceOver65);
+// } else {
+//     console.log("Il tuo biglietto costerà €" + price);
+// }
 
 
