f0323818e747ad7c137a9ead8102926e80e3b88a
Danielateresa <dany.t_87@hotmail.it>
2022-10-07 15:08:15 +0200

    Applied prices by age group

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,4 +12,32 @@ Solo una volta che il milestone 1 sarà completo e funzionante allora realizzere
 Il riepilogo dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
 Questo richiederà un minimo di ricerca.
 */
-alert('ciao')
+
+
+//chiedere all'utente i km da percorrere
+const km = Number(prompt("Quanti km dovrai percorrere?"));
+console.log("km");
+
+//chiedere all'utente l'età del passeggero
+const passengerAge = Number(prompt("Quanti anni ha il passeggero?"));
+console.log("passengerAge");
+
+//calcolare il costo del biglietto in base ai km (0.21 € al km)
+const price = km * 0.21;
+//va applicato uno sconto 10del 20% per i minorenni
+discountMinors = price * 0.20;
+priceMinors = price - discountMinors;
+//va applicato uno sconto del 40% per gli over 65.
+discountOver65 = price * 0.40;
+priceOver65 = price - discountOver65;
+
+
+if (passengerAge < 18) {
+    console.log("Dato che sei minorenne, il tuo biglietto scontato costerà €" + priceMinors);
+} else if (passengerAge > 65) {
+    console.log("Dato che hai più di 65 anni, il tuo biglietto scontato costerà €" + priceOver65);
+} else {
+    console.log("Il tuo biglietto costerà €" + price);
+}
+
+
